f401a37175df6b53b5b4cb84ccfa9ebbebfe97c3
xavier <xavierhoskins@outlook.com>
2020-03-19 13:22:04 -0500

    finished project day 2

--- src/components/planets.js
+++ src/components/planets.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import './planets.css';
+import styled from 'styled-components';
 
+const Planet = styled.div`
+  box-sizing: border-box;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid black;
+  background-color: #747e80;
+  color: white;
+`;
+const PlanetInfo = styled.section`
+  box-sizing: border-box;
+  margin: auto;
+  padding: 20px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  background-color: #2b3e42;
+`;
+const PaddingTop = styled.div`
+  padding-top: 3%;
+`;
+const CircularSquare = styled.img`
+  border-top-left-radius: 50% 50%;
+  border-top-right-radius: 50% 50%;
+  border-bottom-right-radius: 50% 50%;
+  border-bottom-left-radius: 50% 50%;
+`;
 class Planets extends Component {
   //   conststructor(props) {
   //     super(props);
   //     this.state = {};
   //   }
+
   render() {
     console.log(this.props.data);
     return (
-      <div className="Planet">
-        <section className="PlanetInfo">
+      <Planet>
+        <PlanetInfo>
           <h1>{this.props.data.title}</h1>
           <h4>{this.props.data.date}</h4>
-          <img className="circular--square" src={this.props.data.url} alt="" />
-          <div className="paddingtop">
+          <CircularSquare src={this.props.data.url} alt="" />
+          <PaddingTop>
             <b>{this.props.data.explanation}</b>
-          </div>
-        </section>
-      </div>
+          </PaddingTop>
+        </PlanetInfo>
+      </Planet>
     );
   }
 }
